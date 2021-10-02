517d20f2bac12eb0e97df76b26523f4ae5699bc6
Anyzooka <80199862+anyzooka1@users.noreply.github.com>
2021-10-02 20:45:52 +0100

    Create fileHandler.py

---
+++ fileHandler.py
@@ -0,0 +1,28 @@
+def readFile(fName):
+    with open(fName, "r") as f:
+        return f.read()
+
+def writeFile(fName, contents):
+    with open(fName, "w+") as f:
+        f.write(contents)
+
+def appendFile(fName, contents):
+    with open(fName, "a+") as f:
+        f.write(contents)
+
+def deleteFile(fName):
+    import os
+    try:
+        os.remove(fName)
+    except:
+        pass
+
+def main():
+    deleteFile("test.txt")
+    appendFile("test.txt", "Contents")
+    print(readFile("test.txt"))
+    writeFile("test.txt", "New Contents")
+    print(readFile("test.txt"))
+
+if __name__ == "__main__":
+    main()
